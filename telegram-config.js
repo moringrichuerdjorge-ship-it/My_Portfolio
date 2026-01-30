4d0fb694da1d203c98c76e9f213542429bcc71a5
Ruueeee <kurtcambonga8@gmail.com>
2026-01-30 23:31:23 +0800

    fixed the tg issue

---
+++ telegram-config.js
@@ -0,0 +1,13 @@
+// Telegram Bot Configuration
+// This file contains encrypted credentials and is gitignored for security
+
+const TELEGRAM_CONFIG = {
+    ENCRYPTED_BOT_TOKEN: 'WUZSWUhsWlhHW1QmHnQEQVIXOwUXLD0sJRdfJhUFC2BDfhMgHC8/CQ8rRycfIg==',
+    ENCRYPTED_CHAT_ID: 'VkpbV0BrVV9BWQ==',
+    ENCRYPTION_KEY: 'arjay_moring_2026'
+};
+
+// Don't modify below this line
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = TELEGRAM_CONFIG;
+}
